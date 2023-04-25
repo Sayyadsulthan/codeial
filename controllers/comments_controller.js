ee94acdc3a540e6ed493bc5d67fe0a4459663265
Sayyadsulthan <asulthan088@gmail.com>
2023-04-25 23:11:42 +0530

    Deleting a Post (Authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,31 +1,33 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const { post } = require('../routes');
 
-module.exports.create= function(req, res){
+module.exports.create = function (req, res) {
 
-    try{
+    try {
         Post.findById(req.body.post)
-        .then((post)=>{
-            if(post){
-                Comment.create({
-                    content:req.body.content,
-                    post:req.body.post,
-                    user:req.user._id
+            .then((post) => {
+                if (post) {
+                    Comment.create({
+                        content: req.body.content,
+                        post: req.body.post,
+                        user: req.user._id
 
-                })
-                .then((comment)=>{
-                    post.comments.push(comment);
-                    post.save();
+                    })
+                        .then((comment) => {
+                            post.comments.push(comment);
+                            post.save();
 
-                    res.redirect('/');//return to home page 
-                })
+                            res.redirect('/');//return to home page 
+                        })
 
-                .catch((err)=>{
-                    return console.log("error while creating the comment", err);
-                })
-            }
-        })
-    }catch(err){
+                        .catch((err) => {
+                            return console.log("error while creating the comment", err);
+                        })
+                }
+            })
+    } catch (err) {
         console.log(err);
     }
 }
+
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-const Posts = require('../models/post');
+const Comment = require('../models/comment');
 
 
 module.exports.create= function(req, res){
@@ -8,7 +8,7 @@ module.exports.create= function(req, res){
     // if(req.isAuthenticated()){
 
         console.log(req.user);
-            Posts.create({
+            Post.create({
                     content:req.body.content,
                     user: req.user._id
                 })
@@ -21,4 +21,37 @@ module.exports.create= function(req, res){
     // }
      
     
+}
+
+module.exports.destroy = function (req, res) {
+    try {
+        Post.findById(req.params.id)
+            .then((post) => {
+                if (post.user == req.user.id) {
+                    //to delete post
+                    try{
+                        Post.deleteOne(post)//delete the post in Post Schema
+                        .then(()=>{
+                            console.log("post deleted successfully")
+                        })
+                    }catch(err){
+                        console.log("error in deleting a post: ",err);
+                    }
+
+                    //to delete comments related to post
+                    try {
+                        Comment.deleteMany({ post: req.params.id })//deletes many comments from Comment Schema
+                            .then(() => {
+                                return res.redirect('back');
+                            })
+                    } catch (err) {
+                        return console.log("err while deleting comment in post", err);
+                    }
+                }
+            })
+
+    } catch (err) {
+        return res.redirect('back');
+    }
+
 }
