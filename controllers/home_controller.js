a480ac91d51b06621b05ef6a05cf0460cb71fdad
Sayyadsulthan <asulthan088@gmail.com>
2023-04-27 12:45:13 +0530

    added some styles

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,64 +1,31 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-// const  = require('../models/post');
 
+module.exports.home = function (req, res) {
 
-module.exports.home = function(req, res){
-    // console.log(req.user);
-     console.log(`request for, ${req.path}`);
-    // console.log('cookires:', req.cookies);
-
-    //to populate the user of each post
     Post.find({}).populate('user')
-    .populate({
-        path:'comments',
-        populate: {
-            path:'user'
-        }
-    })
-    .exec()
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec()
 
-    .then((posts)=>{
-        User.find({})
-            .then((users)=>{
-                return res.render('home',{
-                    title:"posts",
-                    subtitle: "Home",
-                    posts: posts,
-                    all_users: users
-                });
-            })
-        
-    })
-       .catch((err)=>{
+        .then((posts) => {
+            User.find({})
+                .then((users) => {
+                    return res.render('home', {
+                        title: "posts",
+                        subtitle: "Home",
+                        posts: posts,
+                        all_users: users
+                    });
+                })
+
+        })
+        .catch((err) => {
             return console.log("error while fetching user from home_controller", err);
         })
-
-        
-  
-    
-
-    // Post.find({})
-    //     .then((posts)=>{ 
-
-    //         return res.render('home',{
-    //             title:"posts",
-    //             subtitle: "Home",
-    //             posts: posts
-    //         });
-    //     })
-    //         .catch((err)=>{
-    //             return console.log("error while fetching user from home_controller", err);
-    //         })
-
-    
-    // console.log(`request for, ${req.path}`);
-    // console.log('cookires:', req.cookies);
-    // return res.render('home',{
-    //     title:"posts",
-    //     subtitle: "Home"
-    //     User: user
-    // });
-    
 
 }
