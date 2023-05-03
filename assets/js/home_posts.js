27adf5314bc28cd1f0e38a49234d5359ad25f90c
Sayyadsulthan <asulthan088@gmail.com>
2023-05-03 21:06:13 +0530

    Creating a Post :: Receiving Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,16 +13,54 @@
                 data: newPostForm.serialize(),
                 success: function(data){
                     console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container > ul').prepend(newPost);
                 }, error: function(error){
                     console.log(error.responseText);
-                }
+                }   
             })
         });
     };
 
 
     // method to create the post in DOM
-
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+        <p>
+    
+                <small>
+                    <a class="delete-post-button" href="/posts/destroy/${post.id}">
+                        <i class="fa-sharp fa-solid fa-trash"></i>
+                    </a>
+                </small>
+                
+                    <label for="">
+                    ${post.content}
+                    </label>
+                    <br>
+                    <small>
+                    ${ post.user.name }
+                    
+                    </small>
+        </p>
+    
+        <div class="post-comments">
+    
+                <form action="comments/create" method="post">
+                    <input type="text" name="content" placeholder="type here to add comment">
+                    <input type="hidden" name="post" value="${ post._id }">
+                    <input type="submit" value="Add-comment">
+                </form>
+    
+    
+                    <div class="post-comments-list">
+                        <ul id="posts-commnets-${ post._id }">
+                            
+                        </ul>
+                    </div>
+        </div>
+    </li>`)
+    }
 
     createPost();
 
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,19 +8,20 @@ module.exports.create =async function (req, res) {
     try{
         let post= await Post.create({
             content: req.body.content,
-            user: req.user._id
+            user: req.user._id,
         });
 
         if(req.xhr){
             return res.status(200).json({
                 data: {
-                    post: post
+                    post: post,
                 },
                 message: "Post created!"
+
             })
         }
 
-        req.flash('success', 'post published successfully!')
+        req.flash('success', 'post published!')
              return res.redirect('back'); 
     }catch(err){
         return console.log("error in creating post: ", err);
