6e3aa956933691e9085707c4511fabf1dd766d3d
Sayyadsulthan <asulthan088@gmail.com>
2023-05-05 00:06:25 +0530

    added delete post for every post in DOM using ajax and NOTY message

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -4,6 +4,7 @@
         console.log("hello");
         let newPostForm = $('#new-post-form');
 
+
         newPostForm.submit(function (e) {
             e.preventDefault();
 
@@ -11,23 +12,36 @@
                 type: 'post',
                 url: '/posts/create',
                 data: newPostForm.serialize(),
-                success: function(data){
+                success: function (data) {
                     console.log(data);
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container > ul').prepend(newPost);
 
-                    // to delete post from DOM
+                    // using NOTY for display message
+                    new Noty({
+
+                        theme: 'relax',
+                        type: 'success',
+                        text: "post created! :)",
+                        layout: 'topRight',
+                        timeout: 1500
+        
+                    }).show();
+
+
+                    // // to delete post from DOM
                     deletePost($(' .delete-post-button', newPost))
-                }, error: function(error){
+                }, error: function (error) {
                     console.log(error.responseText);
-                }   
+                }
             })
         });
+
     };
 
 
     // method to create the post in DOM
-    let newPostDom = function(post){
+    let newPostDom = function (post) {
         return $(`<li id="post-${post._id}">
         <p>
     
@@ -42,7 +56,7 @@
                     </label>
                     <br>
                     <small>
-                    ${ post.user.name }
+                    ${post.user.name}
                     
                     </small>
         </p>
@@ -51,13 +65,13 @@
     
                 <form action="comments/create" method="post">
                     <input type="text" name="content" placeholder="type here to add comment">
-                    <input type="hidden" name="post" value="${ post._id }">
+                    <input type="hidden" name="post" value="${post._id}">
                     <input type="submit" value="Add-comment">
                 </form>
     
     
                     <div class="post-comments-list">
-                        <ul id="posts-commnets-${ post._id }">
+                        <ul id="posts-commnets-${post._id}">
                             
                         </ul>
                     </div>
@@ -66,25 +80,48 @@
     }
 
 
+    
+
     // method to delete a post from DOM
-    let deletePost = function(deleteLink){
-        $(deleteLink).click(function(e){
+    let deletePost = function (deleteLink) {
+        $(deleteLink).click(function (e) {
             e.preventDefault();
 
             $.ajax({
                 type: 'get',
                 url: $(deleteLink).prop('href'),
-                success: function(data){    
-                    console.log(data.data);
-                    $(`#post-${ data.data.post_id }`).remove();
-                }, error: function(error){
+                success: async function (data) {
+                    console.log(data);
+                    await $(`#post-${data.data.post_id}`).remove();
+
+                     // using NOTY for display message
+                     new Noty({
+
+                        theme: 'relax',
+                        type: 'error',
+                        text: "post deleted!",
+                        layout: 'topRight',
+                        timeout: 1500
+        
+                    }).show();
+
+                }, error: function (error) {
                     console.log(error.responseText);
                 }
             })
         })
     }
 
-    // deletePostDom();
+    // for traverse all li from DOM #posts-list-container > ul >li
+    let allpost = $('#posts-list-container > ul >li');
+    
+    for (let post of allpost) {
+        // to delete post from DOM
+        // console.log("for inside");
+        console.log($(' .delete-post-button', post," 1"));
+        deletePost($(' .delete-post-button', post))
+    }
+
     createPost();
 
 }
