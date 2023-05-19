9fb5dd1110ae1814333f58f11cc52e718ad2fd7d
Sayyadsulthan <asulthan088@gmail.com>
2023-05-19 23:05:37 +0530

    rendered html or ejs  content to mail

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,12 +3,13 @@ const nodemailer = require('../config/nodemailer');
 
 exports.newComment = (comment)=>{
 
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
     console.log("inside comment out: ", comment);
     nodemailer.transporter.sendMail({
         from:'projecttest62@gmail.com',
         to: comment.user.email,
         subject:'New comment Published',
-        html:`<h1>hey, ${comment.user.name}  your comment published </h1>`
+        html: htmlString
     }, (err, info)=>{
         if(err){console.log("err in sending mail...", err); return};
 
