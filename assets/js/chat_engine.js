b32d9364a12ba8fe09665f03d8c837fd5b81cfb8
Sayyadsulthan <asulthan088@gmail.com>
2023-05-30 19:28:30 +0530

    Send and Receive Messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -13,7 +13,7 @@ class ChatEngine {
     connectionHandler(){
 
         let self = this;
-        
+
         this.socket.on('connect', function(){
             console.log('connection established using sockets...');
 
@@ -27,5 +27,43 @@ class ChatEngine {
                 console.log('user joined..', data);
             })
         });
+
+
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if(msg != ''){
+                self.socket.emit('send_message', {
+                    message:msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if(data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html' : data.message
+            }));
+
+            newMessage.append($('<sub>', {
+                'html' : data.user_email
+            }));
+
+
+            newMessage.addClass(messageType);
+
+            $('#chat-message-list').append(newMessage);
+        })
     }
 }
